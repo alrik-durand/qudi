20c7cd7959aac270ff5de1f3b32ee4b7af00b9cb
Alrik <alrik.durand@gmail.com>
2019-12-30 17:24:32 +0100

    worked on new logic

--- logic/polarisation_dep_logic.py
+++ logic/polarisation_dep_logic.py
@@ -19,8 +19,12 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 
+import time
+import numpy as np
 from core.connector import Connector
 from logic.generic_logic import GenericLogic
+from core.statusvariable import StatusVar
+from core.configoption import ConfigOption
 from qtpy import QtCore
 
 
@@ -33,53 +37,121 @@ class PolarisationDepLogic(GenericLogic):
     counterlogic = Connector(interface='CounterLogic')
     savelogic = Connector(interface='SaveLogic')
     motor = Connector(interface='MotorInterface')
+    motor_axis = ConfigOption(name='motor_axis', default='phi')
+
+    main_channel = ConfigOption(name='main_channel', default=0)
+    secondary_channel = ConfigOption(name='secondary_channel', default=None)
 
     signal_rotation_finished = QtCore.Signal()
     signal_start_rotation = QtCore.Signal()
 
+    _resolution = StatusVar('resolution', 90)
+    _time_per_point = StatusVar('time_per_point', 1)
+
+    _x_axis = []
+    _y_axis = []
+    _y2_axis = []
+
+    sigDataUpdated = QtCore.Signal()
+    sigStateChanged = QtCore.Signal()
+    _stop_requested = False
+
     def on_activate(self):
-        """ Initialisation performed during activation of the module.
-        """
-
-        self._counter_logic = self.counterlogic()
-#        print("Counting device is", self._counting_device)
-
-        self._save_logic = self.savelogic()
-
-        self._hwpmotor = self.motor()
-
-        # Initialise measurement parameters
-        self.scan_length = 360
-        self.scan_speed = 10 #not yet used
-
+        """ Initialisation performed during activation of the module. """
         # Connect signals
-        self.signal_rotation_finished.connect(self.finish_scan, QtCore.Qt.QueuedConnection)
-        self.signal_start_rotation.connect(self.rotate_polarisation, QtCore.Qt.QueuedConnection)
-
+        self.counterlogic().sigCountStatusChanged(self.abort, QtCore.Qt.DirectConnection)
 
     def on_deactivate(self):
-        """ Deinitialisation performed during deactivation of the module.
-        """
+        """ Deinitialisation performed during deactivation of the module. """
         return
 
-    def measure_polarisation_dependence(self):
-        """Do a simple pol dep measurement.
+    @property
+    def motor_constraints(self):
+        return self.motor().get_contraints()[self.motor_axis]
+
+    @property
+    def motor_velocity(self):
+        return self.motor().get_velocity()[self.motor_axis]
+
+    @property
+    def motor_position(self):
+        return self.motor().get_position()[self.motor_axis]
+
+    def set_motor_position(self, value):
+        """ Set the motor to a given position
+
+        @param (float) value: The new position to set
+
+        @return (float): Time (in second) needed to get to this position
         """
+        if self.motor_constraints['pos_mim'] < value < self.motor_constraints['pos_max']:
+            previous_position = self.motor_position
+            self.motor().move_abs({self.motor_axis: value})
+            return abs((previous_position-value)/self.motor_velocity)
 
-        # Set up measurement
-        self._hwpmotor.move_abs(0)
+    @property
+    def resolution(self):
+        return self.resolution
 
-        # configure the countergui
+    @resolution.setter
+    def resolution(self, value):
+        if value > 0 and self.module_state() != 'locked':
+            self.resolution = int(value)
 
+    @property
+    def time_per_point(self):
+        return self._time_per_point
 
-        self._counter_logic.start_saving()
-        self.signal_start_rotation.emit()
+    @time_per_point.setter
+    def time_per_point(self, value):
+        counter_time_per_point = 1/self.counterlogic.get_count_frequency()
+        if counter_time_per_point != 0 and value % counter_time_per_point != 0:
+            self.log.warning('Polarisation measurement time per point must be a multiple of counting logic resolution.')
+        else:
+            if self.module_state() != 'locked':
+                self._time_per_point = value
 
-    def rotate_polarisation(self):
-        self._hwpmotor.move_rel(self.scan_length)
-        self.log.info('rotation finished, saving data')
-        self.signal_rotation_finished.emit()
+    def reset_motor(self, wait=False):
+        """ Reset the motor to its origin
 
-    def finish_scan(self):
-        self._counter_logic.save_data()
-#        self._counter_logic.stopCount()
+        @param (bool) wait: Whether to wait for the motor to be reset before returning
+        """
+        reset_time = self.set_motor_position(0)
+        if wait:
+            time.sleep(reset_time)
+
+    def run(self):
+        """ Runs a polarisation measurement """
+        if self.counterlogic().module_state() == 'idle':
+            self.counterlogic().startCount()
+        self._stop_requested = False
+        self.module_state.lock()
+        self.sigStateChanged.emit()
+        self._x_axis = np.linspace(start=0, stop=180, num=self.resolution)
+        self._y_axis = np.zeroes(self.resolution)
+        if self.secondary_channel is not None:
+            self._y2_axis = np.zeroes(self.resolution)
+        self.sigDataUpdated.emit()
+        self.reset_motor(wait=True)
+        bin_per_step = int(self.counterlogic().get_count_frequency() * self.time_per_point)
+
+        for i, x in enumerate(self._x_axis):
+            move_time = self.set_motor_position(x)
+            time.sleep(move_time)
+            time.sleep(self.time_per_point)
+            self._y_axis[i] = self.counterlogic().countdata[self.main_channel, -bin_per_step:-1].sum()
+            if self.secondary_channel is not None:
+                self._y2_axis[i] = self.counterlogic().countdata[self.secondary_channel, -bin_per_step:-1].sum()
+            if self._stop_requested:
+                break
+            self.sigDataUpdated.emit()
+
+        self.sigDataUpdated.emit()
+        self.reset_motor(wait=True)
+        self.module_state.unlock()
+        self.sigStateChanged.emit()
+
+    def abort(self):
+        """ Abort the acquisition in progress - the motor will reset to zero """
+        if self.module_state() == 'locked':
+            self._stop_requested = True
