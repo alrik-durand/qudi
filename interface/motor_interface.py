2c77f3b21cc7fee1cc80536657f122c6bd54bbce
Alrik <alrik.durand@gmail.com>
2020-06-02 18:53:57 +0200

    completely changed motor interface

--- interface/motor_interface.py
+++ interface/motor_interface.py
@@ -19,159 +19,99 @@ along with Qudi. If not, see <http://www.gnu.org/licenses/>.
 Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
+from enum import Enum
 
-from core.interface import abstract_interface_method
 from core.meta import InterfaceMetaclass
+from core.interface import abstract_interface_method
+from core.interface import ScalarConstraint
+
+
+class Status(Enum):
+    """ Different possible status of the hardware """
+    IDLE = 0
+    MOVING_FORWARD = 1
+    MOVING_BACKWARD = 2
+    MOVING_UNKNOWN = 3  # Some hardware might not know this. The logic will handle it.
+
+
+class AxisConstraints:
+    def __init__(self):
+        self.label = ''
+        self.unit = ''  # 'rad' or 'meter'
+        self.position = ScalarConstraint()
+        self.velocity = ScalarConstraint()
+        self.acceleration = ScalarConstraint()
 
 
 class MotorInterface(metaclass=InterfaceMetaclass):
-    """ This is the Interface class to define the controls for the simple
-        step motor device. The actual hardware implementation might have a
-        different amount of axis. Implement each single axis as 'private'
-        methods for the hardware class, which get called by the general method.
+    """ This is the Interface class to define the controls for a multi axis step motor device.
+
     """
 
     @abstract_interface_method
     def get_constraints(self):
-        """ Retrieve the hardware constrains from the motor device.
+        """ Retrieve the hardware fixed constrains
 
-        @return dict: dict with constraints for the magnet hardware. These
-                      constraints will be passed via the logic to the GUI so
-                      that proper display elements with boundary conditions
-                      could be made.
-
-        Provides all the constraints for each axis of a motorized stage
-        (like total travel distance, velocity, ...)
-        Each axis has its own dictionary, where the label is used as the
-        identifier throughout the whole module. The dictionaries for each axis
-        are again grouped together in a constraints dictionary in the form
-
-            {'<label_axis0>': axis0 }
-
-        where axis0 is again a dict with the possible values defined below. The
-        possible keys in the constraint are defined here in the interface file.
-        If the hardware does not support the values for the constraints, then
-        insert just None. If you are not sure about the meaning, look in other
-        hardware files to get an impression.
-
-        Example of how a return dict with constraints might look like:
-        ==============================================================
-
-        constraints = {}
-
-        axis0 = {}
-        axis0['label'] = 'x'    # it is very crucial that this label coincides
-                                # with the label set in the config.
-        axis0['unit'] = 'm'     # the SI units, only possible m or degree
-        axis0['ramp'] = ['Sinus','Linear'], # a possible list of ramps
-        axis0['pos_min'] = 0,
-        axis0['pos_max'] = 100,  # that is basically the traveling range
-        axis0['pos_step'] = 100,
-        axis0['vel_min'] = 0,
-        axis0['vel_max'] = 100,
-        axis0['vel_step'] = 0.01,
-        axis0['acc_min'] = 0.1
-        axis0['acc_max'] = 0.0
-        axis0['acc_step'] = 0.0
-
-        axis1 = {}
-        axis1['label'] = 'phi'   that axis label should be obtained from config
-        axis1['unit'] = 'degree'        # the SI units
-        axis1['ramp'] = ['Sinus','Trapez'], # a possible list of ramps
-        axis1['pos_min'] = 0,
-        axis1['pos_max'] = 360,  # that is basically the traveling range
-        axis1['pos_step'] = 100,
-        axis1['vel_min'] = 1,
-        axis1['vel_max'] = 20,
-        axis1['vel_step'] = 0.1,
-        axis1['acc_min'] = None
-        axis1['acc_max'] = None
-        axis1['acc_step'] = None
-
-        # assign the parameter container for x to a name which will identify it
-        constraints[axis0['label']] = axis0
-        constraints[axis1['label']] = axis1
+        @return list(AxisConstraints): A list of AxisConstraints representing the constraints of each axis
         """
         pass
 
     @abstract_interface_method
-    def move_rel(self,  param_dict):
+    def move_rel(self, axis, value):
         """ Moves stage in given direction (relative movement)
 
-        @param dict param_dict: dictionary, which passes all the relevant
-                                parameters, which should be changed. Usage:
-                                 {'axis_label': <the-abs-pos-value>}.
-                                 'axis_label' must correspond to a label given
-                                 to one of the axis.
+        @param (str) axis : Label of the axis
+        @param (float) value : Relative distance to move
 
-        A smart idea would be to ask the position after the movement.
-
-        @return int: error code (0:OK, -1:error)
+        Method returns immediately. Use get_status for help.
         """
         pass
 
     @abstract_interface_method
-    def move_abs(self, param_dict):
+    def move_abs(self, axis, value):
         """ Moves stage to absolute position (absolute movement)
 
-        @param dict param_dict: dictionary, which passes all the relevant
-                                parameters, which should be changed. Usage:
-                                 {'axis_label': <the-abs-pos-value>}.
-                                 'axis_label' must correspond to a label given
-                                 to one of the axis.
+        @param (str) axis : Label of the axis
+        @param (float) value : Position to set
 
-        @return int: error code (0:OK, -1:error)
         """
         pass
 
     @abstract_interface_method
-    def abort(self):
+    def abort(self, axis):
         """ Stops movement of the stage
 
-        @return int: error code (0:OK, -1:error)
+        @param (str) axis : Label of the axis
         """
         pass
 
     @abstract_interface_method
-    def get_pos(self, param_list=None):
-        """ Gets current position of the stage arms
+    def get_pos(self, axis):
+        """ Gets current position of an axis
 
-        @param list param_list: optional, if a specific position of an axis
-                                is desired, then the labels of the needed
-                                axis should be passed in the param_list.
-                                If nothing is passed, then from each axis the
-                                position is asked.
+        @param (str) axis : Label of the axis
 
-        @return dict: with keys being the axis labels and item the current
-                      position.
+        @return (float): Current position of the axis
+
+        The current position should match the physical value if possible, not the setpoint value
         """
         pass
 
     @abstract_interface_method
-    def get_status(self, param_list=None):
-        """ Get the status of the position
+    def get_status(self, axis):
+        """ Get the status of the given axis
 
-        @param list param_list: optional, if a specific status of an axis
-                                is desired, then the labels of the needed
-                                axis should be passed in the param_list.
-                                If nothing is passed, then from each axis the
-                                status is asked.
+        @param (str) axis : Label of the axis
 
-        @return dict: with the axis label as key and the status number as item.
+        @return (Status): Status object representing the current status
         """
         pass
 
     @abstract_interface_method
-    def calibrate(self, param_list=None):
+    def calibrate(self, axis):
         """ Calibrates the stage.
 
-        @param dict param_list: param_list: optional, if a specific calibration
-                                of an axis is desired, then the labels of the
-                                needed axis should be passed in the param_list.
-                                If nothing is passed, then all connected axis
-                                will be calibrated.
-
-        @return int: error code (0:OK, -1:error)
+        @param (str) axis : Label of the axis
 
         After calibration the stage moves to home position which will be the
         zero point for the passed axis. The calibration procedure will be
@@ -180,29 +120,20 @@ class MotorInterface(metaclass=InterfaceMetaclass):
         pass
 
     @abstract_interface_method
-    def get_velocity(self, param_list=None):
-        """ Gets the current velocity for all connected axes.
+    def get_velocity(self, axis):
+        """ Gets the current velocity of an axis
 
-        @param dict param_list: optional, if a specific velocity of an axis
-                                is desired, then the labels of the needed
-                                axis should be passed as the param_list.
-                                If nothing is passed, then from each axis the
-                                velocity is asked.
+        @param (str) axis : Label of the axis
 
-        @return dict : with the axis label as key and the velocity as item.
+        @return (float): The velocity in unit/s
         """
         pass
 
     @abstract_interface_method
-    def set_velocity(self, param_dict):
-        """ Write new value for velocity.
+    def set_velocity(self, axis, value):
+        """ Set the velocity of an axis
 
-        @param dict param_dict: dictionary, which passes all the relevant
-                                parameters, which should be changed. Usage:
-                                 {'axis_label': <the-velocity-value>}.
-                                 'axis_label' must correspond to a label given
-                                 to one of the axis.
-
-        @return int: error code (0:OK, -1:error)
+        @param (str) axis : Label of the axis
+        @param (float) value: The velocity in unit/s
         """
         pass
